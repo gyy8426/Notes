17a053e88a90716dd60cebecbc514f5abde3a00b
gyy8426 <842651507@qq.com>
2017-11-30 20:34:32 +0800

    Update Over_the_wall.sh

--- Over_the_wall.sh
+++ Over_the_wall.sh
@@ -1,2 +1,2 @@
-sudo sslocal -s ss.tansong.pw -p 54322  -l 1080 -k "**********" -t 600 -m chacha20 -d start;
+sudo sslocal -s "IP" -p "PWD"  -l 1080 -k "**********" -t 600 -m chacha20 -d start;
 proxychains wget www.google.com.hk;
