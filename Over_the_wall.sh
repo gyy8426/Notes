ee77efdbac3a9e4a66abe91349f9a0934aa95390
gyy8426 <842651507@qq.com>
2018-07-31 20:34:59 +0800

    Update Over_the_wall.sh

--- Over_the_wall.sh
+++ Over_the_wall.sh
@@ -1,2 +1,4 @@
 sudo sslocal -s "IP" -p "PWD"  -l 1080 -k "**********" -t 600 -m chacha20 -d start;
 proxychains wget www.google.com.hk;
+install shadowsocks && proxychains
+set proxychains as http://gohom.win/2016/10/24/cmd_proxy/
